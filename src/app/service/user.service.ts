e9ccbc9508124cfd438d2e0d66c8539eeb6c3283
Turinnash <istvangooz@gmail.com>
2021-03-16 22:33:32 +0100

    first commit

---
+++ src/app/service/user.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  endpoint: string = 'http://localhost:3000/users';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+   * Get all users from the database.
+   * @returns on observable with all users.
+   */
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.endpoint}`);
+  }
+
+  /**
+   * Get a specified user from the database by id.
+   * @param id {number} user id.
+   * @returns an observable with a user object.
+   */
+  get(id: number): Observable<User> {
+    return this.http.get<User>(`${this.endpoint}/${id}`);
+  }
+
+  /**
+   * Delete a user from the database.
+   * The method is: this.http.delete
+   */
+  
+  delete(user: User):void {
+    this.http.delete<User>(`${this.endpoint}/${user.id}`).subscribe(
+      () => this.getAll()
+    );
+  }
+  
+
+  /**
+   * Create a user in the database.
+   * The method is: this.http.post
+   */
+  
+
+
+
+  /**
+   * Update a user in the database.
+   * The method is: this.http.patch
+   */
+
+}
